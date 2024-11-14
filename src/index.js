4a27c5fcac6bae947cc84ce36d6448debd7c2c2d
nontakorn <nontakorn_chris@hotmail.com>
2024-11-14 11:58:05 -0500

    done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import About from "./components/About";
 
 ReactDOM.render(<App />, document.getElementById("root"));
